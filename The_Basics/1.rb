f036e2c3947c51d29e0a8785979aa3cb270e2806
Jake Guzman <jacobguz26@gmail.com>
2022-08-26 05:49:37 -0700

    Completed exercises 1 and 2 in The Basics chapter.

---
+++ The_Basics/1.rb
@@ -0,0 +1,1 @@
+puts "Jake" + " Guzman"
---
+++ The_Basics/2.rb
@@ -0,0 +1,4 @@
+puts 1234 / 1000 #thousands place
+puts 1234 % 1000 / 100 #hundreds place
+puts 1234 % 1000 % 100 / 10  #tens place
+puts 1234 % 1000 % 100 % 10 #ones place
